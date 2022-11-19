19369baeaaf1871a1b892613b77a8dbf52f81fef
unknown <jensennics.dlrs@gmail.com>
2022-11-19 12:21:26 +0800

    I added text audio for the program to greet aand guide the user.

--- text_to_speech.py
+++ text_to_speech.py
@@ -8,26 +8,33 @@ import pyttsx3
 #Second Step: Print the options:
                 #Upload a text file
                 #Type the text
+
+engine = pyttsx3.init()
 print ("\n====================================================================")
-print ("       Hi There! This program can read the text you want to hear.")
+print ("    Hi There! This program can read the text you want to hear.")
 print ("====================================================================")
+engine.say ("Hi There! This program can read the text you want to hear.")
+engine.runAndWait()
 print ("\nOptions")
 print ("1 --> Directly type the text")
 print ("2 --> Upload a Text File\n")
 print ("====================================================================\n")
 
 #Second Step: Option Conditions
-engine = pyttsx3.init()
-userInputfunction = int (input ("What option do you want to try? ")) #Third Step: Ask user for input
+engine.say ("What option do you want to try? Enter only the number")
+engine.runAndWait()
+print ("What option do you want to try?\n")
+userInputfunction = int (input ("Enter only the number: ")) #Third Step: Ask user for input
+
 if userInputfunction == 1: 
-    directType = input ("Enter the text you want me to read: ")
+    directType = input ("\nEnter the text you want me to read: ")
     engine.say (directType) #Fourth Step: code for text to speech
     engine.runAndWait()
 else:
-    inputFilename = input ("Enter the file name of the text file you want me to read: ")
+    inputFilename = input ("\nEnter the file name of the text file you want me to read: ")
     file = open (inputFilename, 'r', encoding = "UTF-8")
     text = file.read()
-    print(text)
+    print(f"\n{text}")
     engine.say (text)
     engine.runAndWait()
     file.close()
