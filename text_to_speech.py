50a546b49d976f3dd7778a55cf65dc058cee383d
unknown <jensennics.dlrs@gmail.com>
2022-11-19 10:10:45 +0800

    I already installed the modules I needed for the prokect, and listed my initial steps for the program.

---
+++ text_to_speech.py
@@ -0,0 +1,12 @@
+#Seatwork 4 in DSA
+
+#Program: Text to Speech using pyttsx3
+#Video inspiration: https://www.youtube.com/watch?v=Rjk2D7XKpvI
+
+#First Step: Import pyttsx3
+#Second Step: Print the options:
+                #Upload a text file
+                #Type the text
+#Second Step: Option Conditions
+#Third Step: Ask user for input
+#Fourth Step: Code for text to speech
