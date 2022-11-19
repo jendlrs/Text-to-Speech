4f351b5ec760e484d9584e1ff5b476f0f63ffde5
unknown <jensennics.dlrs@gmail.com>
2022-11-19 10:45:09 +0800

    I displayed the menu option for the program and I import  the module I needed.

--- text_to_speech.py
+++ text_to_speech.py
@@ -4,9 +4,24 @@
 #Video inspiration: https://www.youtube.com/watch?v=Rjk2D7XKpvI
 
 #First Step: Import pyttsx3
+import pyttsx3
 #Second Step: Print the options:
                 #Upload a text file
                 #Type the text
+print ("\n====================================================================")
+print ("       Hi There! This program can read the text you want to hear.")
+print ("====================================================================")
+print ("\nOptions")
+print ("1 --> Directly type the text")
+print ("2 --> Upload a Text File\n")
+print ("====================================================================\n")
 #Second Step: Option Conditions
+engine = pyttsx3.init()
+userInputfunction = int (input ("What option do you want to try? "))
+if userInputfunction == 1:
+    directType = input ("Enter the text you want me to read: ")
+    engine.say (directType)
+    engine.runAndWait()
+
 #Third Step: Ask user for input
 #Fourth Step: Code for text to speech
